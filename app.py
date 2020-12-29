7861c0db5c718c246223e7e982a5e2fd899ccc46
kg29475 <keya.ghonasgi@austin.utexas.edu>
2020-12-29 17:37:51 -0600

    reading game params and obstacle seeds, if no obstacle seeds available, game will not work.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import send_file
 from flask import request
 from flask import url_for
 from flask import render_template, redirect
+import json
 
 app = Flask(__name__)
 
@@ -14,8 +15,29 @@ def hello():
 @app.route('/get_data', methods=['POST'])
 def get_data():
     data = request.get_json(silent=True)
-    print(data['logname'])
+    # print(data)
+    # print(data['logname'])
     filename = data['logname'];
     with open(f'{filename}.txt', "w") as test:
         test.write(str(data['logdata']))
     return {"Success":True}
+
+@app.route('/send_gameparam', methods=['POST'])
+def send_gameparam():
+    data = request.get_json(silent = True)
+    pid = str(data['player_no'])
+    print(pid)
+    num = str(data['attempt'])
+    
+    data = json.load(open(f'JSONFiles/Player_{pid}/GameParams.json','r'))
+    return data[num]
+
+@app.route('/send_gameseeds', methods=['POST'])
+def send_gameseeds():
+    data = request.get_json(silent = True)
+    pid = str(data['player_no'])
+    print(pid)
+    num = str(data['attempt'])
+    
+    data = json.load(open(f'JSONFiles/Player_{pid}/GameSeeds_{num}.json','r'))
+    return data
